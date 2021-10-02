fbda7d0a45eabc985c1e57ce86969eca3c7e61af
Psoccrates Stansberry <psoccratesstudio@gmail.com>
2021-10-01 22:36:18 -0400

    Updated nav, created modal.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) => 
-item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded")}));
+// Get modal element, open and close modal
+const modal = document.getElementById('navModal');
+const openModalBtn = document.getElementById('openModalImage');
+const closeModalBtn = document.getElementById('closeModalImage');
+
+//Listen for opening, closing, outside clicks
+openModalBtn.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
+window.addEventListener('click', clickOutside);
+
+//Function to open modal
+function openModal() {
+    modal.style.display = 'block';
+    openModalImage.style.display = 'none';
+    closeModalImage.style.display = 'inline-flex';
+
+}
+
+//Function to close modal 
+function closeModal() {
+    modal.style.display = 'none';
+    closeModalImage.style.display = 'none';
+    openModalImage.style.display = 'inline-flex';
+}
+
+//Function to close modal if outside click
+function clickOutside(outsideClicks){
+    if(outsideClicks.target === modal){
+        closeModal();
+    }
+}
